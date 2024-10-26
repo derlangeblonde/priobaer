c13e9ee4f3b6b5d76a47a094e5ec43cf897dc478
Jonathan Schill <jon.schill@posteo.de>
2024-10-26 19:47:45 +0200

    feat: add user and session management

---
+++ middleware.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"slices"
+	"strconv"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+const UserIdSessionKey = "userId"
+
+func AuthMiddleware(db *gorm.DB) gin.HandlerFunc {
+	whitelist := []string {
+		"/favicon.png",
+		"/favicon.ico",
+		"/health",
+		"/login",
+		"/register",
+	}
+
+	return func(c *gin.Context) {
+		if slices.Contains(whitelist, c.FullPath()){
+			c.Next()
+
+			return
+		}
+
+		userId, err := getUserIdFromSession(c)
+
+		if err != nil {
+			c.Redirect(http.StatusSeeOther, "/login")
+
+			return
+		}
+
+		user := User{ID: userId}
+		result := db.First(&user)
+
+		if result.Error != nil {
+			c.Redirect(http.StatusSeeOther, "/login")
+
+			return
+		}
+
+		c.Next()
+	}
+
+}
+
+func getUserIdFromSession(c *gin.Context) (int, error) {
+	session := sessions.Default(c)
+
+	userIdRaw := session.Get(UserIdSessionKey)
+
+	if userIdRaw == nil {
+		return -1, errors.New("UserId not set")
+	}
+
+	userId, err := strconv.Atoi(userIdRaw.(string))
+
+	return userId, err
+}
+
+
---
+++ view/load_templates.go
@@ -0,0 +1,44 @@
+package view
+
+import (
+	"embed"
+	_ "embed"
+	"fmt"
+	"html/template"
+	"io/fs"
+	"path/filepath"
+	"strings"
+)
+
+//go:embed **/*.html
+var templateFS embed.FS
+
+func LoadTemplate() (*template.Template, error) {
+	
+	templates := template.New("")
+	err := fs.WalkDir(templateFS, ".", func(path string, d fs.DirEntry, err error) error {
+
+		if !d.IsDir() && strings.HasSuffix(path, ".tmpl.html") {
+			templateContent, err := templateFS.ReadFile(path)
+
+			if err != nil {
+				return err
+			}
+
+			name := filepath.ToSlash(path)
+
+			name = strings.Replace(name, ".tmpl.html", "", 1)
+
+			fmt.Printf("- Registered new template: %s\n", name)
+
+			_, err = templates.New(name).Parse(string(templateContent))
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	return templates, err
+}
