deb3cec9ae437f9b790e395ae9936f3564b6857a
Jonathan Schill <jon.schill@posteo.de>
2024-12-13 21:08:25 +0100

    feat: trigger PUT request to assignments via drag-and-drop

--- static/script.js
+++ static/script.js
@@ -7,3 +7,30 @@ window.addEventListener("beforeunload", function (event) {
         event.returnValue = '';
     }
 });
+
+/**
+    *@param {DragEvent} e
+    */
+function dragStart(e) {
+    e.dataTransfer.setData("text", e.target.id)
+}
+
+/**
+    *@param {DragEvent} e
+    */
+function drop(e) {
+    let participantId = e.dataTransfer.getData("text").split("-")[1]
+    let courseId = e.target.id.split("-")[1]
+
+    console.log(participantId)
+    console.log(courseId)
+
+    htmx.ajax("PUT", "/assignments", {"target": "#" + e.dataTransfer.getData("text"), "values": {"participant-id": participantId, "course-id": courseId}})
+}
+
+/**
+    *@param {DragEvent} e
+    */
+function allowDrop(e) {
+    e.preventDefault()
+}
