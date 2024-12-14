099b67b9a440b67736844eab78af45d201946fc8
Jonathan Schill <jon.schill@posteo.de>
2024-12-14 12:40:42 +0100

    feat: make random-entitiy factories seeded

--- cmd_test/factory.go
+++ cmd_test/factory.go
@@ -1,24 +1,34 @@
 package cmdtest
 
 import (
-	"math/rand/v2"
+	"strconv"
 
 	"github.com/google/uuid"
+	"golang.org/x/exp/rand"
 	"softbaer.dev/ass/model"
 )
 
-func RandomCourse() model.Course {
-	name := uuid.New()
+var namespace uuid.UUID = uuid.MustParse("123e4567-e89b-12d3-a456-426614174000")
+var intSeed uint64 = 69420
+var rnd *rand.Rand = rand.New(rand.NewSource(intSeed))
 
-	minCap := rand.IntN(30)
-	maxCap := minCap + rand.IntN(30)
+func SeededUUID() uuid.UUID {
+	oneTimeSeedStr := strconv.Itoa(rnd.Int())
+	return uuid.NewMD5(namespace, []byte(oneTimeSeedStr))
+}
+
+func RandomCourse() model.Course {
+	name := SeededUUID()
+
+	minCap := rnd.Intn(30)
+	maxCap := minCap + rnd.Intn(30)
 
 	return model.Course{Name: name.String(), MinCapacity: minCap, MaxCapacity: maxCap}
 }
 
 func RandomParticipant() model.Participant {
-	prename := uuid.New()
-	surname := uuid.New()
+	prename := SeededUUID()
+	surname := SeededUUID()
 
 	return model.Participant{Prename: prename.String(), Surname: surname.String()}
 }
