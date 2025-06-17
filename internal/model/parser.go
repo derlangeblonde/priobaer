c8c020120bb362f738f6c47c5897940b8e0f4811
Jonathan Schill <jonathan.schill@tu-dortmund.de>
2025-06-17 22:56:16 +0200

    factor parsing of solution

---
+++ internal/model/parser.go
@@ -0,0 +1,59 @@
+package model
+
+import (
+	"fmt"
+	"strconv"
+
+	"softbaer.dev/ass/internal/z3"
+)
+
+type solutionParser struct {
+	coursesById     map[int]Course
+	participantsById map[int]Participant
+}
+
+func newSolutionParser() *solutionParser {
+	return &solutionParser{coursesById: make(map[int]Course, 0), participantsById: make(map[int]Participant, 0)}
+}
+
+func (s *solutionParser) registerLookups(p Participant, c Course) {
+	s.participantsById[p.ID] = p
+	s.coursesById[c.ID] = c
+}
+
+func (s *solutionParser) parse(solution map[string]*z3.AST) (assignments []Assignment, err error) {
+	for varName, solutionStr := range solution {
+		solution, err := strconv.Atoi(solutionStr.String())
+
+		if err != nil {
+			return assignments, fmt.Errorf("Could not parse assigned solution. varName: %s, solution: %s", varName, solutionStr)
+		}
+
+		if solution != 1 {
+			continue
+		}
+
+		assignmentId, err := parseAssignmentId(varName)
+
+		if err != nil {
+			return assignments, err
+		}
+
+		course, ok := s.coursesById[assignmentId.CourseId]
+
+		if !ok {
+			return assignments, fmt.Errorf("Did not find course with id: %d", assignmentId.CourseId)
+		}
+
+		participant, ok := s.participantsById[assignmentId.ParticipantId]
+
+		if !ok {
+			return assignments, fmt.Errorf("Did not find participant with id: %d", assignmentId.ParticipantId)
+		}
+
+		assignment := Assignment{Course: course, Participant: participant}
+		assignments = append(assignments, assignment)
+	}
+
+	return
+}
