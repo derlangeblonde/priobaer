3d940053187aa30cab55e552023e6398d11a3992
Jonathan Schill <jon.schill@posteo.de>
2025-10-12 15:23:20 +0200

    remove code that lives no in dedicated handler

--- internal/app/scenario_handler.go
+++ internal/app/scenario_handler.go
@@ -2,15 +2,12 @@ package app
 
 import (
 	"database/sql"
-	"errors"
 	"log/slog"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 	"softbaer.dev/ass/internal/crypt"
 	"softbaer.dev/ass/internal/domain"
-	"softbaer.dev/ass/internal/model"
 	"softbaer.dev/ass/internal/ui"
 )
 
@@ -29,54 +26,6 @@ func ScenarioIndex(c *gin.Context) {
 	if err != nil {
 		slog.Error("Bad request on AssignmentsIndex", "err", err)
 		return
-	}
-
-	if req.Solve {
-		err := db.Transaction(
-			func(tx *gorm.DB) error {
-				var availableCourses []model.Course
-				if result := tx.Preload("Participants").Find(&availableCourses); result.Error != nil {
-					return result.Error
-				}
-
-				var unassignedParticipants []model.Participant
-				if result := tx.Where("course_id is null").Find(&unassignedParticipants); result.Error != nil {
-					return result.Error
-				}
-
-				var relevantPriorities []model.Priority
-				// TODO: optimize querying
-				if result := tx.Preload("Participant").Preload("Course").Where("participant_id in ?", model.ParticipantIds(unassignedParticipants)).Find(&relevantPriorities); result.Error != nil {
-					return result.Error
-				}
-
-				assignments, err := model.SolveAssignment(relevantPriorities)
-				if err != nil {
-					return err
-				}
-
-				err = model.ApplyAssignments(tx, assignments)
-				if err != nil {
-					return err
-				}
-
-				return nil
-			},
-		)
-
-		if errors.Is(err, model.NotSolvable) {
-			slog.Info("Could not solve assignment", "err", err)
-			c.HTML(http.StatusOK, "dialogs/not-solvable", gin.H{})
-
-			return
-		}
-
-		if err != nil {
-			slog.Error("Error while trying to solve assignment", "err", err)
-			c.AbortWithStatus(500)
-
-			return
-		}
 	}
 
 	scenario, err := domain.LoadScenario(db, secret)
