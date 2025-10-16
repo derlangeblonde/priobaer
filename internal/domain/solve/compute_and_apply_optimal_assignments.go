f522535979d64b7905aa567b720120379c31ac90
Jonathan Schill <jon.schill@posteo.de>
2025-10-16 22:09:52 +0200

    add doc-comment

--- internal/domain/solve/compute_and_apply_optimal_assignments.go
+++ internal/domain/solve/compute_and_apply_optimal_assignments.go
@@ -4,6 +4,9 @@ import (
 	"gorm.io/gorm"
 )
 
+// ComputeAndApplyOptimalAssignments reads current scenario from the DB, computes which assignments would be optimal
+// to satisfy the prioritization of the still unassigned participants and writes these assignments to the DB.
+// Prefer passing a transaction as DB-handle so that partial updates will be rolled back in case of an error.
 func ComputeAndApplyOptimalAssignments(tx *gorm.DB) error {
 	priorityConstraints, err := queryPriorityConstraints(tx)
 	if err != nil {
